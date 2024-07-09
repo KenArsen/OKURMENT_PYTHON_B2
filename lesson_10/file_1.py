f6c46919eb8a66e2923e35d124f11fc99cd62a9b
KenArsen <kenzhegulov2002@gmail.com>
2024-07-09 21:40:50 +0600

    Added new lesson

---
+++ lesson_10/file_1.py
@@ -0,0 +1,23 @@
+# while <условие>:
+#     operator 1
+#     operator 1
+#     operator 1
+#     operator 1
+
+# break
+# continue
+s = 0
+while s < 10:
+    n = int(input("Введите одно число: "))
+
+    s += n
+
+    print (f"Общая сумма = {s}")
+else:
+    print("While toluk ishtep buttu!")
+
+
+
+
+
+ 
