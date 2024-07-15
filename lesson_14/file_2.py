00d6655ecf50baaed2f9f48c614378d6bf15fc6a
KenArsen <kenzhegulov2002@gmail.com>
2024-07-15 18:04:56 +0600

    fixed dict_gen

--- lesson_14/file_2.py
+++ lesson_14/file_2.py
@@ -7,4 +7,5 @@ a = [i for i in range(10)]
 
 list_gen = [int(i) for i in range(10)]
 set_gen = {i for i in "OkurmenOkurmen"}
-dict_gen = {f"{i}: {j}" for i, j in enumerate("Okurmen")}
+dict_gen = {i: j for i, j in enumerate("Okurmen")}
+print(dict_gen)
